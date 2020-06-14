f9ffdfb39caae3b70b8cf6ae2471ea69fcdb865a
Dillon Christensen <j.dillon.christensen@gmail.com>
2020-06-14 17:11:32 -0500

    added some shortcodes for formatting from another file

--- print.py
+++ print.py
@@ -4,23 +4,36 @@ import math
 MAX_PRINT_LEN = 32
 
 def repeat(s, n):
-    return "".join([s for _ in range(n)])
+    return "".join([s for _ in range(int(n))])
 
 def line_of(s = "="):
     return repeat(s, MAX_PRINT_LEN)
 
 def center(s):
     s_len = len(s)
-    if s_len <= MAX_PRINT_LEN:
+    if s_len < MAX_PRINT_LEN:
         start = math.floor((MAX_PRINT_LEN - s_len) / 2)
         return repeat(" ", start) + s
     return s
 
-def print_ln(l):
-    l = l.strip()
-    if len(l) <= MAX_PRINT_LEN:
-        return l
+def banner(s, banner_char = "="):
+    s = " {} ".format(s)
+    s_len = len(s)
+    if s_len < MAX_PRINT_LEN:
+        start = math.floor((MAX_PRINT_LEN - s_len) / 2)
+        return repeat("=", start) + s + repeat(banner_char, MAX_PRINT_LEN - s_len - start)
+    return s
 
+def right_justify(s):
+    if len(s) < MAX_PRINT_LEN:
+        return repeat(" ", MAX_PRINT_LEN - len(s)) + s
+    return s
+
+def print_ln(l):
+    if len(l) <= MAX_PRINT_LEN:
+        return l.rstrip()
+
+    has_checkbox = l.startswith("( ) ")
     new_l = ""
     current_len = 0
     for w in l.split(" "):
@@ -28,11 +41,23 @@ def print_ln(l):
             new_l = w if len(new_l) < 1 else new_l + " {}".format(w)
             current_len += len(w) + 1
         else:
-            new_l += "\n{}".format(w)
-            current_len = len(w)
-    return new_l
+            padding = "    " if has_checkbox else ""
+            new_l += "\n{}{}".format(padding, w)
+            current_len = len(w) + len(padding)
+    return new_l.rstrip()
+
+def process_line(l):
+    if l.startswith("=>"):
+        l = right_justify(l[2:].strip())
+    elif l.startswith("<=>"):
+        l = center(l[3:].strip())
+    elif l.startswith("[=]"):
+        l = banner(l[3:].strip())
+    elif l.startswith("..."):
+        l = line_of(l[3:].strip())
+    return print_ln(l)
 
 
 for line in sys.stdin:
-    print(print_ln(line))
+    print(process_line(line))
 
